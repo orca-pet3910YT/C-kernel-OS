cb19b6fe10d5e72e6086b9c2c9b8700d58550d78
orca.pet3910YT <169390367+orca-pet3910YT@users.noreply.github.com>
2026-03-05 08:03:09 +0100

    Update vga.h

--- include/vga.h
+++ include/vga.h
@@ -3,7 +3,7 @@
 #define VGA_H
 void set_cursor_pos(int row, int col);
 void clear_screen();
-void set_ftimestamp();
+void set_ftimestamp(double timestamp, char *buf);
 void putc(char c);
 void puts(const char *s);
 void printk(const char *str);
