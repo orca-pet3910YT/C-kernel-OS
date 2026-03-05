feb2eee983f29c6b4e4ab3f68d9f9527bbf3179f
orca.pet3910YT <169390367+orca-pet3910YT@users.noreply.github.com>
2026-03-05 08:06:23 +0100

    Fix function types in string.h

--- include/string.h
+++ include/string.h
@@ -1,6 +1,6 @@
 #include <stddef.h>
-int strcmp();
-size_t strlen();
-char *strcat();
-char *strcpy();
-void *memset();
+int strcmp(const char *a, const char *b);
+size_t strlen(const char *a);
+char *strcat(char *to, const char *from);
+char *strcpy(char *to, const char *from);
+void *memset(void *to, int what, unsigned int count);
