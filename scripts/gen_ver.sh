b6ce25dc85cebcac0c0a5c5e7c36044e3fff9fe1
orca.pet3910YT <orca.pet3910@gmail.com>
2026-03-19 20:37:45 +0100

    Update gen_ver.sh

--- scripts/gen_ver.sh
+++ scripts/gen_ver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 date_bld=$(date)
 echo "   GEN  include/generated/__GENVER.h"
+mkdir -p include/generated
 tr -d '\r' > include/generated/__GENVER.h <<EOF
 #ifndef CKOS_BLD
 #define CKOS_BLD "$(whoami)@$(hostname) at ${date_bld} on $(uname -s)"
